02ad89e5adecf465b31dffd3e5d3090ed2986613
mly12s <mly12s@gmail.com>
2017-11-16 15:27:10 -0600

    init commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+import Search_Bar from '../containers/Search_Bar'
+import Weather_List from '../containers/weather_list'
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+      	<Search_Bar />
+      	<Weather_List />
+      </div>
     );
   }
 }
